a981d4e8ef7c5dec932ef306b0e625f8bbb51df9
Cameron_Thornton <cameronthornton2001@gmail.com>
2023-11-01 16:03:11 -0700

    moved Jupyter notebook contents into python file

--- elastic_search.py
+++ elastic_search.py
@@ -1,14 +1,106 @@
 from elasticsearch import Elasticsearch
+import pandas as pd
+from elasticsearch.helpers import bulk
+#docker run --rm -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.7.0
 
-# Connect to Elasticsearch
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+#Navigate to Elasticsearch connect to local host
+es = Elasticsearch("http://localhost:9200")
+es.info().body
 
-# Create an index
-index_name = 'interactions_index'
-es.indices.create(index=index_name, ignore=400)
+#Read data file and place into pandas dataframe
+df = (
+    pd.read_csv("scripps-research-test/Data/sample_interaction_data.tsv",sep="\t",lineterminator='\n')
+    .dropna()
+    # .sample(5000, random_state=42)
+    .reset_index()
+)
+#pd.set_option('display.max_colwidth', None)
+print(df.head())
 
-# Bulk insert the interaction objects
-interactions = parse_data_file('data.csv')
+#Create index and mappings
+mappings = {
+    "properties": {
+        "index": {"type": "long "},
+        "gene_name": {"type": "text"}, 
+        "gene_claim_name": {"type": "text"},
+        "entrez_id": {"type": "long "},
+        "interaction_claim_source": {"type": "text"},
+        "interaction_types": {"type": "text"},
+        "drug_claim_primary_name": {"type": "text"},    
+        "drug_name": {"type": "text"},      
+        "drug_concept_id": {"type": "text"},
+        "interaction_group_score": {"type": "long "},    
+        "PMIDs": {"type": "long "}
+    }
+}
 
-for interaction in interactions:
-    es.index(index=index_name, doc_type='_doc', id=interaction['_id'], body=interaction)
+#es.indices.create(index="medications", mappings=mappings)
+
+
+
+#Add data to index
+for i, row in df.iterrows():
+    doc = {
+        "index": row["index"],
+        "gene_name": row["gene_name"], 
+        "gene_claim_name": row["gene_claim_name"],
+        "entrez_id": row["entrez_id"],
+        "interaction_claim_source": row["interaction_claim_source"],
+        "interaction_types": row["interaction_types"],
+        "drug_claim_primary_name": row["drug_claim_primary_name"],    
+        "drug_name": row["drug_name"],      
+        "drug_concept_id": row["drug_concept_id"],
+        "interaction_group_score": row["interaction_group_score"],    
+        "PMIDs": row["PMIDs"]
+    }
+            
+es.index(index="medications", id=i, document=doc)
+
+
+
+
+#causes key error
+#Store bulk data in list
+bulk_data = []
+for i,row in df.iterrows():
+    bulk_data.append(
+        {
+            "_index": "medications",
+            "_id": i,
+            "_source": {        
+                "index": row["index"],
+                "gene_name": row["gene_name"], 
+                "gene_claim_name": row["gene_claim_name"],
+                "entrez_id": row["entrez_id"],
+                "interaction_claim_source": row["interaction_claim_source"],
+                "interaction_types": row["interaction_types"],
+                "drug_claim_primary_name": row["drug_claim_primary_name"],    
+                "drug_name": row["drug_name"],      
+                "drug_concept_id": row["drug_concept_id"],
+                "interaction_group_score": row["interaction_group_score"],    
+                "PMIDs": row["PMIDs"],
+            }
+        }
+    )
+bulk(es, bulk_data)
+
+
+es.indices.refresh(index="medications")
+es.cat.count(index="medications", format="json")
+
+
+
+resp = es.search(
+    index="medications",
+    query={
+            "bool": {
+                "must": {
+                    "match_phrase": {
+                        "gene_name": "ROS1",
+                    }
+                },
+                "filter": {"bool": {"must_not": {"match_phrase": {"director": "roman polanski"}}}},
+            },
+        },            
+)
+resp.body
