972c427c122470cb83d47471be6ee374abed5602
Cameron_Thornton <cameronthornton2001@gmail.com>
2023-11-06 01:51:28 -0800

    Added _id and field search functionality

--- elastic_search_API.py
+++ elastic_search_API.py
@@ -3,6 +3,7 @@
 from elasticsearch import Elasticsearch
 from Data.data_processing import unzip_file, read_file
 import pandas as pd
+import json
 
 def connect_client():
     """
@@ -18,7 +19,6 @@ def connect_client():
     # API key should have cluster monitor rights
     client.info()
     print("Connection Status is",client.ping())
-
     return client
 
 def index_data(data_file,client):
@@ -36,37 +36,108 @@ def index_data(data_file,client):
         .dropna()
         .reset_index()
     )
-
+    print(df.head())
     documents = []
     for i in range(len(df)):
         documents.append(
             { "index": { "_index": "search-medications", "_id": str(i)}}
         )
         documents.append(df.iloc[i].to_dict())
-
     client.bulk(operations=documents, pipeline="ent-search-generic-ingestion")
     return documents, client
 
-def index_search(key_word):
-    """
-    Searches for documents containing the given keyword in the index.
 
+# def index_search(key_word):
+#     """
+#     Searches for documents containing the given keyword in the index.
+
+#     Args:
+#         key_word (str): The keyword to be searched for.
+
+#     Returns:
+#         dict: The search results.
+#     """
+#     client = connect_client()
+#     output = client.search(index="search-medications", q=key_word)
+#     return output
+
+
+def get_interaction_by_id(id,mappings):
+    """
+    Returns the interaction object with the specified _id field.
+    
     Args:
-        key_word (str): The keyword to be searched for.
+        id (str): The _id value to search for in the interactions index.
 
     Returns:
-        dict: The search results.
+        str: A JSON string representing the interaction object that matches the specified _id.
+
+    Raises:
+        HTTPException: If no interaction object is found that matches the specified _id.
     """
     client = connect_client()
-    return client.search(index="search-medications", q=key_word)
+    result = client.search(index="search-medications", body={"query": {"match": {"_id": id}}})
+    hits = result['hits']['hits']
+    json_object = json.dumps(hits[0]['_source'], indent = 4)
+    if len(hits) == 0:
+        return json({"message": "Interaction not found"}), 404
+    else:
+        return json_object
+    
+
+def get_interaction_by_field(field_value,mappings):
+    """
+    Returns all interaction objects that have the specified value in the specified field.
+
+    Args:
+        field_value (str): The value to search for in the specified field.
+
+    Returns:
+        str: A JSON string representing the interaction objects that match the search criteria.
+
+    Raises:
+        HTTPException: If no interaction objects are found that match the search criteria.
+    """
+    client = connect_client()
+    result = client.search(index="search-medications", q=field_value)
+    json_object = json.dumps(mappings, indent = 4)
+    hits = result['hits']['hits']
+    if len(hits) == 0:
+        return json({"message": "Interaction not found"}), 404
+    else:
+        print(json_object)
+        return json_object
+
+
 
 def main():
+    mappings = {
+        "properties": {
+            "gene_name": {"type": "text"}, 
+            "gene_claim_name": {"type": "text"},
+            "entrez_id": {"type": "long "},
+            "interaction_claim_source": {"type": "text"},
+            "interaction_types": {"type": "text"},
+            "drug_claim_primary_name": {"type": "text"},    
+            "drug_name": {"type": "text"},      
+            "drug_concept_id": {"type": "text"},
+            "interaction_group_score": {"type": "long "},    
+            "PMIDs": {"type": "long "}
+        }}
     data_file = "Data/sample_interaction_data.tsv"
     client = connect_client()
     documents, client = index_data(data_file, client)
-    key_word = "ROS1"
-    search_results = index_search(key_word)
-    print(search_results)
+    # key_word = "ROS1"
+    # search_results = index_search(key_word)
+    # print(search_results)
+    search_method = input("Enter 1 to search by id field, 2 to search by objects within fields: ")
+    if search_method == "1":
+        id = input("Enter id: ")
+        get_interaction_by_id(id,mappings)
+    elif search_method == "2":
+        field_value = input("Enter field value: ")
+        get_interaction_by_field(field_value,mappings)
+
 
 if __name__ == "__main__":
     main()
